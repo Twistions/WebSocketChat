84a6586efbc446145d9cac1ecfa45d1645607580
BuildTools <unconfigured@null.spigotmc.org>
2019-06-25 02:01:47 +0300

    Add project files.

---
+++ Client/MainClient.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using WebSocketSharp;
+namespace Client
+{
+    class Program
+    {
+
+        static string GUID;
+        static WebSocket Ws;
+        static string Name;
+        static void Main(string[] args)
+        {
+            baştan:
+            Console.Write("Name: ");
+            string input1 = Console.ReadLine();
+            if (input1.Length > 5 && input1.Length < 15)
+            {
+                Name = input1;
+            }
+            else
+            {
+                Console.WriteLine("Name should be bigger than 5 characters and smaller then 15 characters.");
+                goto baştan;
+            }
+            Ws = new WebSocket("ws://192.168.1.29/Chat");
+            Ws.OnMessage += Ws_OnMessage;
+            Ws.Connect();
+            Ws.OnClose += Ws_OnClose;
+            Console.WriteLine("Connected to the server.");
+            Console.WriteLine("");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                Dictionary<string, string> dic = new Dictionary<string, string>();
+                dic.Add("command", "message");
+                dic.Add("message", input);
+                dic.Add("guid", GUID);
+                Ws.Send(ObjectToByteArray(dic));
+            }
+            
+        }
+
+        private static void Ws_OnClose(object sender, CloseEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private static void Ws_OnMessage(object sender, MessageEventArgs e)
+        {
+            CommandManager(e.RawData);
+        }
+
+        static void CommandManager(byte[] data)
+        {
+            Dictionary<string, string> dic = ByteArrayToDictionary(data);
+            string command;
+            string from;
+            string message;
+            dic.TryGetValue("command", out command);
+            switch (command)
+            {
+                default:
+                    break;
+                case "setguid":
+                    dic.TryGetValue("guid", out GUID);
+                    Dictionary<string, string> setname = new Dictionary<string, string>();
+                    setname.Add("command", "setname");
+                    setname.Add("guid", GUID);
+                    setname.Add("name", Name);
+                    Console.WriteLine("Your guid: " + GUID);
+                    Ws.Send(ObjectToByteArray(setname));
+                    break;
+                case "message":
+                    dic.TryGetValue("from", out from);
+                    dic.TryGetValue("message", out message);
+                    Console.WriteLine(from + ": " + message);
+                    break;
+            }
+        }
+
+        static byte[] ObjectToByteArray(object obj)
+        {
+            if (obj == null)
+                return null;
+            BinaryFormatter bf = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bf.Serialize(ms, obj);
+                return ms.ToArray();
+            }
+        }
+
+        static Dictionary<string, string> ByteArrayToDictionary(byte[] bytes)
+        {
+            if (bytes.Length > 0)
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    ms.Write(bytes);
+                    ms.Position = 0;
+                    return (Dictionary<string, string>)bf.Deserialize(ms);
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Server/MainServer.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Server
+{
+    class Program : WebSocketBehavior
+    {
+        
+        static WebSocketServer Ws;
+        static List<User> Users = new List<User>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server starting...");
+            Ws = new WebSocketServer("ws://192.168.1.29");
+            Ws.AddWebSocketService<Program>("/Chat");
+            Ws.Start();
+            Console.WriteLine("Websocket server started on 192.168.1.29/Chat");
+            while (true)
+            {
+                Console.WriteLine("Type DONE to close server.");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "done")
+                {
+                    Ws.Stop();
+                    break;
+                }
+            }
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            CommandManager(e.RawData, Context.WebSocket);
+        }
+
+
+        static byte[] ObjectToByteArray(object obj)
+        {
+            if (obj == null)
+                return null;
+            BinaryFormatter bf = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bf.Serialize(ms, obj);
+                return ms.ToArray();
+            }
+        }
+
+        static Dictionary<string,string> ByteArrayToDictionary(byte[] bytes)
+        {
+            try
+            {
+                if (bytes.Length > 0)
+                {
+                    BinaryFormatter bf = new BinaryFormatter();
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        ms.Write(bytes);
+                        ms.Position = 0;
+                        return (Dictionary<string, string>)bf.Deserialize(ms);
+                    }
+                }
+                return null;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        
+        static void CommandManager(byte[] data, WebSocket ws)
+        {
+            Dictionary<string, string> dic = ByteArrayToDictionary(data);
+            string command = null;
+            string name;
+            string guid;
+            string message;
+            dic.TryGetValue("command", out command);
+            if (command != null)
+            {
+                switch (command)
+                {
+                    default:
+                        ws.Close();
+                        break;
+                    case "setname":
+                        dic.TryGetValue("name", out name);
+                        dic.TryGetValue("guid", out guid);
+                        if (name.Length > 5 && name.Length < 15)
+                        {
+                            foreach (var item in Users)
+                            {
+                                if (item.GUID == guid)
+                                {
+                                    item.Name = name;
+                                    item.CanText = true;
+                                    Console.WriteLine("New user connected with GUID: " + guid + " Name: " + name);
+                                    break;
+                                }
+                            }
+                        }
+                        break;
+                    case "message":
+                        User user = null;
+                        dic.TryGetValue("guid", out guid);
+                        dic.TryGetValue("message", out message);
+                        foreach (var item in Users)
+                        {
+                            if (item.GUID == guid)
+                            {
+                                user = item;
+                            }
+                        }
+                        if (user != null)
+                        {
+                            if (user.CanText)
+                            {
+                                WebSocketServiceHost host;
+                                Ws.WebSocketServices.TryGetServiceHost("/Chat", out host);
+                                Dictionary<string, string> dicmessage = new Dictionary<string, string>();
+                                dicmessage.Add("command", "message");
+                                dicmessage.Add("from", user.Name);
+                                dicmessage.Add("message", message);
+                                host.Sessions.Broadcast(ObjectToByteArray(dicmessage));
+                            }
+                        }
+
+                        break;
+                }
+            }
+            else
+            {
+                ws.Close();
+            }
+        }
+
+        protected override void OnOpen()
+        {
+            string guid = Guid.NewGuid().ToString();
+            WebSocket ws = Context.WebSocket;
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("command", "setguid");
+            dic.Add("guid", guid);
+            ws.Send(ObjectToByteArray(dic));
+            Users.Add(new User(null, guid));
+        }
+    }
+
+    class User
+    {
+        public string Name { get; set; }
+        public string GUID { get; set; }
+
+        public bool CanText { get; set; }
+
+        public User(string name, string guid)
+        {
+            Name = name;
+            GUID = guid;
+            CanText = false;
+        }
+    }
+}
